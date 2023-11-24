8d8ac024e45efc12a6aec825a982f043e186dd6e
Lais Costa de Oliveira <lais.oliveira@gruporecursos.intranet>
2023-11-24 14:29:16 -0300

    API RESTful

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -14,30 +14,35 @@ import jakarta.persistence.Table;
 @Table(name = "TB_PRODUCTS")
 public class ProductModel implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID idProduct;
 	private String name;
-	private BigDecimal decimal;
+	private BigDecimal value;
+
 	public UUID getIdProduct() {
 		return idProduct;
 	}
+
 	public void setIdProduct(UUID idProduct) {
 		this.idProduct = idProduct;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public BigDecimal getDecimal() {
-		return decimal;
-	}
-	public void setDecimal(BigDecimal decimal) {
-		this.decimal = decimal;
+
+	public BigDecimal getValue() {
+		return value;
 	}
 
-	
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
 }
